599644fca017b7eaeb555a87fed781bef6fe3e6b
Bryce Schmidt <brycesch@gmail.com>
2015-07-24 11:52:12 -0500

    added route for token expiration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Devise::Oauth2Providable::Engine.routes.draw do
   root :to => "authorizations#new"
   resources :authorizations, :only => :create
   match 'authorize' => 'authorizations#new', via: [:get, :post]
-  resource :token, :only => :create
+  resource :token, :only => [:create, :destroy]
+  match 'token' => 'tokens#destroy', via: [:delete]
 end
